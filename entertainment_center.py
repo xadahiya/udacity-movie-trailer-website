ae3ae432ead1df934856f82dd8186e8e2c8de394
Akshay Dahiya <xadahiya@gmail.com>
2016-10-18 20:18:29 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,11 +3,16 @@ import fresh_tomatoes
 
 # A list that stores all the movie objects
 movie_list = [
-    media.movie("Iron Man", "http://www.gstatic.com/tv/thumb/movieposters/170620/p170620_p_v8_ah.jpg", "https://www.youtube.com/watch?v=8hYlB38asDY"),
-    media.movie("Doctor Strange","http://cdn3-www.superherohype.com/assets/uploads/gallery/doctor-strange/cf5nqhcxiae59i9-jpg-large.jpg","https://www.youtube.com/watch?v=HSzx-zryEgM"),
-    media.movie("Avengers", "http://t1.gstatic.com/images?q=tbn:ANd9GcTp0qlAoWcOOswIkL_qpjYzJqCCDmWXiBzCXiqbE43Obo8c0Z-s", "https://www.youtube.com/watch?v=eOrNdBpGMv8")
+    media.movie("Iron Man",
+                "http://www.gstatic.com/tv/thumb/movieposters/170620/p170620_p_v8_ah.jpg",  # noqa
+                "https://www.youtube.com/watch?v=8hYlB38asDY"),
+    media.movie("Doctor Strange",
+                "http://cdn3-www.superherohype.com/assets/uploads/gallery/doctor-strange/cf5nqhcxiae59i9-jpg-large.jpg",  # noqa
+                "https://www.youtube.com/watch?v=HSzx-zryEgM"),
+    media.movie("Avengers",
+                "http://t1.gstatic.com/images?q=tbn:ANd9GcTp0qlAoWcOOswIkL_qpjYzJqCCDmWXiBzCXiqbE43Obo8c0Z-s",  # noqa
+                "https://www.youtube.com/watch?v=eOrNdBpGMv8")
 ]
 
-# function to render the page in browser using fresh_tomatoes
-def render_page():
-    fresh_tomatoes.open_movies_page(movie_list)
+# function call  to render the page in browser using fresh_tomatoes
+fresh_tomatoes.open_movies_page(movie_list)
