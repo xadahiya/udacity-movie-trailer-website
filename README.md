ea8767a72f87a6ed49e3f2066177d009634c51d2
xadahiya <xadahiya@gmail.com>
2016-10-18 19:08:06 +0530

    p1 ready for initial submission

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project 1 Udacity Fullstack developer nanodegree
+
+### How to run the project
+  * Clone the project to your local directory
+  * Open your favourite python interpreter in project directory
+  * Import entertainment_center
+  * Call the render()
+  * That's pretty much it!
---
+++ media.py
@@ -0,0 +1,11 @@
+class movie:
+    """ A simple movie class that takes movie title, movie poster image url and movie trailer url."""
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+
+    # retruns movie title
+    def __str__(self):
+        return self.title
